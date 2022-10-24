3da198d848dcd52bc2a11f12c1a587086c0e8fd6
Maureen Ng'ang'a <104794175+akisami@users.noreply.github.com>
2022-10-24 13:53:16 +0100

    Website
    This includes the index HTML, master CSS and Java script app controller code for my front-end website.

---
+++ app.js
@@ -0,0 +1,8 @@
+var app = angular.module("menuApp", []);
+
+app.controller("appController", ($scope) => {
+
+    // dayOfWeek = "TUESDAY";
+    // offerings = [ "Cheese Omelette", "California Rolls", "Parisian Cafe au Lait", "Garden Salad", "Osaka Green Tea"];
+
+});
